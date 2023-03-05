67a31b635c0907141a6e0d2372169760f3434fa5
txmx4 <123477527+txmx4@users.noreply.github.com>
2023-03-05 15:57:53 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -70,7 +70,7 @@ const {
 
 let banUser = JSON.parse(fs.readFileSync('./database/banUser.json'));
 let banchat = JSON.parse(fs.readFileSync('./database/banChat.json'));
-
+let premium = JSON.parse(fs.readFileSync('./database/premiumuser.json'));
  let _limit = JSON.parse(fs.readFileSync('./storage/user/limit.json'));
  let _buruan = JSON.parse(fs.readFileSync('./storage/user/bounty.json'));
  let _darahOrg = JSON.parse(fs.readFileSync('./storage/user/blood.json'))
