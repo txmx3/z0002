f32baecfd235814cd843525a7292511055f39ec9
Max2t2 <127627763+Max2t2@users.noreply.github.com>
2023-04-01 16:24:42 +0200

    Update tiktokaudio.js

--- Commands/Media/tiktokaudio.js
+++ Commands/Media/tiktokaudio.js
@@ -17,7 +17,7 @@ module.exports = {
           return m.reply("Please provide a valid Tiktok link!")
         }
 
-        require('../../lib/tiktokScrapper').Tiktok(args[0]).then( data => {
+        require('../../lib2/tiktokScrapper').Tiktok(args[0]).then( data => {
         Miku.sendMessage(m.from, { audio: { url: data.audio },mimetype: "audio/mpeg",},{ quoted: m })
         })
         },
