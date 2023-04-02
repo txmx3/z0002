b974d3deaf4542b77f845f911af1fecfe624ecd7
Max2t2 <127627763+Max2t2@users.noreply.github.com>
2023-04-02 02:31:53 +0200

    Update buy.js

--- Commands/RPG/buy.js
+++ Commands/RPG/buy.js
@@ -1,4 +1,4 @@
-const config = require('../../config');
+const config = require('../../config2');
 const { player } = require("../../Database/rpgschema.js");
 const eco = require('discord-mongoose-economy')
 const ty = eco.connect(config.mongodb);
