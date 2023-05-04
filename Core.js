899749e87ea4d30efd76bbee15c33cf1340dd374
txmx3 <116598451+txmx3@users.noreply.github.com>
2023-05-04 12:55:05 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -49,15 +49,17 @@ const xfarrapi = require('xfarr-api')
 const { hentai } = require('./lib/scraper2.js')
 let { msgFilter } = require('./lib/antispam')
 const { mediafireDl } = require('./lib/mediafire.js')
+/*
 const {
 getRegisteredRandomId,
 addRegisteredUser,
 createSerial,
 checkRegisteredUser
 } = require('.register.js')
-
+/*
 global.prem = require("./database/premium.json")
-/*const command = ${prefix}
+/*
+const command = ${prefix}
 const can = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 					const tik = can[Math.floor(Math.random() * can.length)]
  Miku.sendMessage(from, { text: `*${command}*\n\nName : ${q}\nAnswer : *${tik}%*` }, { quoted: m })
